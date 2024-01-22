30e290fc29b8d7ab215d0e297f8c9a67420584e4
kenkenm43 <kenkenm43@gmail.com>
2024-01-22 23:54:34 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading>The Wild Oasis</Heading>
+        <Button onClick={() => alert("Check in")}>Check in</Button>
+
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  );
+}
+
+export default App;
